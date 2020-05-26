7c010ba8c67e9c30da489cd6bd8e43aaf648ca11
i-pvg <parthvineelgarg3024@gmail.com>
2020-05-26 23:42:53 +0530

    Updates

---
+++ precache-manifest.1bcd58240bb58698143bdfad0bc03c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e660c5a8f500f2d40305fb625cf5c031",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a99cf0b8d2bd31c18831",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e416789baba60e007de",
+    "url": "/Portfolio/static/js/2.a617a02f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.a617a02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99cf0b8d2bd31c18831",
+    "url": "/Portfolio/static/js/main.12df13fc.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  }
+]);
